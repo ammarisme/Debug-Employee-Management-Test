436594dd2b570dc3fed225d704ae6972750652eb
abameerdeen <a.b.ameerdeen@gmail.com>
2018-01-18 09:25:35 +0530

    Removing everythin

--- ProductionControllSystem/Areas/Employee/Models/EmployeesViewModels.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace FrontEnd.Areas.Employees.Models
-{
-    
-}
--- ProductionControllSystem/obj/Release/Package/PackageTmp/Tasks.js
+++
@@ -1,4 +0,0 @@
-﻿//TODO: MachineOut -Based on the output liquid type of distillation filter recievers
-//TODO: Update must work for 0
-
-//TODO: Add language support
